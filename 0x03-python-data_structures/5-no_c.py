ed58d006b67f0e26f350ec1732f5d18e2b18fbf2
Noon47714 <noon47714@gmail.com>
2023-12-05 09:09:48 -0800

    task5

--- 0x03-python-data_structures/5-no_c.py
+++ 0x03-python-data_structures/5-no_c.py
@@ -1,6 +1,5 @@
-#!/usr/bin/env python3
-no_c = __import__('5-no_c').no_c
-
-print(no_c("Best School"))
-print(no_c("Chicago"))
-print(no_c("C is fun!"))
+#!/usr/bin/python3
+def no_c(my_string):
+    new_string = my_string.translate({ord('c'): None})
+    new_string = new_string.translate({ord('C'): None})
+    return new_string
