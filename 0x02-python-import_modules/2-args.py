8426f7b5aef2901a83d6a04818beb09be15fecd2
Noon47714 <noon47714@gmail.com>
2023-12-06 08:10:56 -0800

    task2

--- 0x02-python-import_modules/2-args.py
+++ 0x02-python-import_modules/2-args.py
@@ -1,13 +1,8 @@
 #!/usr/bin/python3
 if __name__ == "__main__":
     import sys
-
-    count = len(sys.argv) - 1
-    if sum = 0:
-        print("0 arguments.")
-    elif sum == 1
-        print("1 argument:")
-    else:
-        print("{} arguments:".format (sum))
-    for i in range(sum):
-        print("{} : {}".format(i + 1, sys.argv[i + 1]))
+    count = len(sys.argv)
+    sum = 0
+    for i in range(count - 1):
+        sum += int(sys.argv[i + 1])
+        print("{}".format(sum))
