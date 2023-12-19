a40d19977dcfd4049245aa445375574b27622a57
Noon47714 <noon47714@gmail.com>
2023-12-19 10:57:14 -0800

    tak1

---
+++ 0x06-python-classes/1-square.py
@@ -0,0 +1,16 @@
+#!/usr/bin/python3
+"""define class"""
+
+
+class Square:
+    """ a square class
+    Attributes
+    __size (int): size of a side of the square
+    """
+    def __init__(self, size):
+        """initalizes attributes
+        Args:
+            size (int): size of a side of the square
+            Returns: None
+            """
+            self.__size = size
