ee773228086743df0c4ed44e265921babff84eed
Noon47714 <noon47714@gmail.com>
2024-01-09 11:57:27 -0800

    task1

--- 0x0B-python-input_output/1-write_file.py
+++ 0x0B-python-input_output/1-write_file.py
@@ -7,8 +7,8 @@ def write_file(filename="", text=""):
     Args:
         filename: the name of the file
         text: the text we want to write into the file.
-        Returns:
-            the number of characters written.
-        """
-        with open(filename, "w", encoding="utf-8") as my_file:
-            return my_file.write(text)
+    Returns:
+        the number of characters written.
+    """
+    with open(filename, "w", encoding="utf-8") as my_file:
+        return my_file.write(text)
