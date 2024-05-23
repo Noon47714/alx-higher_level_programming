f0e27c0ea654a83b14ec0c79bef3ad7a04f589f8
Noon47714 <noon47714@gmail.com>
2024-05-23 10:31:36 -0700

    task14

--- 0x0D-SQL_introduction/14-average.sql
+++ 0x0D-SQL_introduction/14-average.sql
@@ -1,3 +1,2 @@
--- lists the number of records
-SELECT `score`, COUNT(*) AS `number` FROM `second_table`
-GROUP BY `score` ORDER BY `number` DESC;
+-- compute the average
+SELECT AVG(`score`) AS `average` FROM `second_table`;
