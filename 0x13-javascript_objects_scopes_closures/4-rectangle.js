2ea58f4581829aebe832aadb563efa9acac97433
Noon47714 <noon47714@gmail.com>
2024-06-19 13:34:48 -0700

    task5

--- 0x13-javascript_objects_scopes_closures/4-rectangle.js
+++ 0x13-javascript_objects_scopes_closures/4-rectangle.js
@@ -16,6 +16,17 @@ class Rectangle {
       console.log(s);
     }
   }
+
+  rotate () {
+    const x = this.height;
+    this.height = this.width;
+    this.width = x;
+  }
+
+  double () {
+    this.height *= 2;
+    this.width *= 2;
+  }
 }
 
 module.exports = Rectangle;
